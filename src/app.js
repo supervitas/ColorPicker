abb11449ff516eb734e049e5d76031f3bb9753a4
vitalynikolaev <nikolaev.vitally@yandex.ru>
2017-10-09 19:40:31 +0300

    need small refactor.

--- src/app.js
+++ src/app.js
@@ -1,115 +1,158 @@
-import "./app.css";
+import './app.css';
 import * as THREE from 'three';
 import ColorPickVert from './ColorPick.vert';
 import ColorPickFrag from './ColorPick.frag';
 
 class App {
-    constructor() {
-        this._canvasContainer = document.getElementById('canvas-container');
-        
-        this._scene = new THREE.Scene();        
-        this._camera = this._createCamera();            
-        this._renderer = this._createRenderer();        
-        this._pickingRenderTarget = this._createColorPickingRT();   
-        this._pickingScene = new THREE.Scene();
+	constructor() {
+		this._canvasContainer = document.getElementById('canvas-container');
 
-        this._texture = this._loadTexture();
+		this._scene = new THREE.Scene();
+		this._camera = this._createCamera();
+		this._renderer = this._createRenderer();
+		this._pickingRenderTarget = this._createColorPickingRT();
+		this._pickingScene = new THREE.Scene();
 
-        this._canvasContainer.appendChild(this._renderer.domElement);            
+		this._texture = this._loadTexture();
 
-        this._cubes = [];
-        this._addCubes();
+		this._canvasContainer.appendChild(this._renderer.domElement);
 
-        this._mouse = new THREE.Vector2();
+		this._cubes = [];
+		this._pickingObjects = [];
+		this._addCubes();
 
-        this._render = this.render.bind(this);
+		this._mouse = new THREE.Vector2();
+		this._pixelBuffer = new Uint8Array(4);
 
-        this._addListeners();
-        this._render();
-    }
+		this._render = this.render.bind(this);
 
-    _addListeners() {
-        window.addEventListener("click", this._onMouseMove.bind(this), false);
-    }
-    _onMouseMove(event) {         
-        this._mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        this._mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        
-        this._checkIntersections();
-    }
+		this._addListeners();
+		this._render();
+	}
 
-    _loadTexture() {
-        const texture = new THREE.TextureLoader().load("src/test_texture.jpg");
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set(1, 1);
-        return texture;
-    }
+	_addListeners() {
+		window.addEventListener('mousemove', this._onMouseMove.bind(this), false);
+	}
+	_onMouseMove(event) {
+		this._mouse.x = event.clientX;
+		this._mouse.y = event.clientY;
+		this.pick();
+	}
 
-    _createColorPickingRT() {
-        const renderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-        pickingRenderTarget.texture.generateMipmaps = false;
-        pickingRenderTarget.texture.minFilter = THREE.NearestFilter;
+	_loadTexture() {
+		const texture = new THREE.TextureLoader().load('src/test_texture.jpg');
+		texture.wrapS = THREE.RepeatWrapping;
+		texture.wrapT = THREE.RepeatWrapping;
+		texture.repeat.set(1, 1);
 
-        return renderTarget;
-    }
+		return texture;
+	}
 
-    _createRenderer() {
-        const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
+	_createColorPickingRT() {
+		const pickingRenderTarget = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight,
+			{ minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter });
 
-        return renderer;
-    }
+		return pickingRenderTarget;
+	}
 
-    _createCamera() {
-        const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 15;
+	_createRenderer() {
+		const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+		renderer.setPixelRatio(window.devicePixelRatio);
+		renderer.setSize(window.innerWidth, window.innerHeight);
 
-        return camera;
-    }
+		return renderer;
+	}
 
-    _addCubes(count = 10) {
-        const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1 );
-        const boxMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map: this._texture });
+	_createCamera() {
+		const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+		camera.position.z = 15;
 
-        const negativeRandom = () => {
-            const neg = Math.random() < 0.5 ? -1 : 1
-            return neg;
-        };
-        
-        for (let i = 0; i < count; i++) {
-            const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-            
-            cube.position.set(negativeRandom() * Math.random() * i * 2, negativeRandom() * Math.random() * i * 2, 0);
+		return camera;
+	}
 
-            this._cubes.push(cube);
-            this._scene.add(cube);                        
-        }
-    }
+	_addCubes(count = 10) {
+		const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1 );
+		const boxMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, map: this._texture });
 
-    _rotateCubes() {
-        for (const cube of this._cubes) {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-        }
-    }
+		const pickingMaterial = new THREE.RawShaderMaterial( {
+			vertexShader: ColorPickVert,
+			fragmentShader: ColorPickFrag,
+			uniforms: {
+				pickingColor: {
+					value: new THREE.Color()
+				}
+			}
+		});
 
-    render() {
-        requestAnimationFrame(this._render);
-                
-        this._rotateCubes();        
-        this._renderer.render(this._scene, this._camera);        
-    }
+		const negativeRandom = () => {
+			const neg = Math.random() < 0.5 ? -1 : 1;
+			return neg;
+		};
 
-    _checkIntersections() {
-        this._raycaster.setFromCamera(this._mouse, this._camera);
-        const intersects = this._raycaster.intersectObjects(this._cubes);
+		for (let i = 0; i < count; i++) {
+			const cube = new THREE.Mesh(boxGeometry, boxMaterial);
 
-        for (const intersected of intersects) {
-            
-        }       
-    }
+			cube.position.set(negativeRandom() * Math.random() * i * 2, negativeRandom() * Math.random() * i * 2, 0);
+
+			this._cubes.push(cube);
+			this._scene.add(cube);
+
+			const pickingObject = cube.clone();
+			pickingObject.material = pickingMaterial.clone();
+			pickingObject.material.uniforms.pickingColor.value.setHex(i + 1);
+
+			this._pickingObjects[i] = pickingObject;
+
+
+			this._pickingScene.add(pickingObject);
+		}
+	}
+
+	_rotateCubes() {
+		for (let i = 0; i < this._cubes.length; i++) {
+			this._cubes[i].rotation.x += 0.01;
+			this._pickingObjects[i].rotation.x += 0.01;
+		}
+	}
+
+	render() {
+		requestAnimationFrame(this._render);
+
+		this._renderer.autoClear = true;
+
+		const clearColor = this._renderer.getClearColor().clone();
+
+		this._renderer.clear(true, true, true);
+
+		this._renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
+		this._renderer.clearTarget(this._pickingRenderTarget, true, true, true);
+		this._renderer.render(this._pickingScene, this._camera, this._pickingRenderTarget);
+		this._renderer.context.finish();
+
+
+		this._renderer.setClearColor(clearColor, 0.0);
+
+
+		this._rotateCubes();
+
+		this._renderer.render(this._scene, this._camera);
+
+	}
+
+	pick() {
+		this._renderer.readRenderTargetPixels(
+			this._pickingRenderTarget,
+			this._mouse.x,
+			this._pickingRenderTarget.height - this._mouse.y,
+			1,
+			1,
+			this._pixelBuffer
+		);
+
+		console.log(this._pixelBuffer)
+		// interpret the pixel as an ID
+		// console.log(this._pixelBuffer);
+	}
 }
 
 new App();
